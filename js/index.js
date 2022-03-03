f064ad97483da66792b2ee5ad445370f73621742
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2022-03-03 22:23:59 +0300

    created responsive design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 'use strict'
+
+function goUp() {
+    let timeOut;
+    let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if(top > 0) {
+        window.scrollBy(0, -100);
+        timeOut = setTimeout('goUp()', 20);
+    } else {
+        clearTimeout(timeOut);
+    }
+}
